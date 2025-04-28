257efbbdea582575b037e38b940dc82951de8bbc
Admin <ivancovs982@gmail.com>
2025-04-28 23:35:56 +0300

    Добавьте файлы проекта.

---
+++ app.js
@@ -0,0 +1,29 @@
+﻿function Fibonacci(limit) {
+    this.limit = limit;
+
+    this[Symbol.iterator] = function () {
+        let a = 1, b = 1
+
+        return {
+            next: () => {
+                if (a > this.limit) {
+                    return { done: true };
+                } else {
+                    const value = a;
+                    [a, b] = [b, a+b]
+                    return { done: false, value: value };
+                }
+            }
+        }
+    }
+}
+
+const fib = new Fibonacci(50);
+
+console.log('=== iterator of Fibonacci ===');
+let iterator = fib[Symbol.iterator]();
+let res = iterator.next();
+while (!res.done) {
+    console.log(res.value);
+    res = iterator.next();
+}
